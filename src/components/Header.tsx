d0eb356988d64a55980b4f4d72029be7c8d95c00
com12300000 <com12300000@gmail.com>
2024-09-29 15:28:47 +0900

    ヘッダーのひな形完成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './css/Header.css'
+import { useNavigate } from 'react-router-dom'
+import imageName from './img/kkrn_icon_user_6.png'
+import { app } from './Firebase'
+
+// ログインしているユーザー情報を取得
+
+
 
 const Header = () => {
+  const navigate = useNavigate()
+
   return (
-    <div>
-      header
+    <div className='header'>
+      <div className='header-area'>
+        <h1 className='title'><a href='/'>みんなの数学質問箱</a></h1>
+        <nav className='header-nav'>
+          <ul>
+            <li><a href='/'>トップ</a></li>
+            <li><a href='/'>マイページ</a></li>
+          </ul>
+          <div className='acount'>
+            <div className='img-area'>
+              <img className='plofile-img' src={imageName} alt='アカウント' />
+            </div>
+            <div className='user-info'> unknown さん</div>
+        </div>
+        </nav>
+      </div>
+      
     </div>
   )
 }
--- src/components/TopPage.tsx
+++ src/components/TopPage.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from './Header'
 
 const TopPage = () => {
   return (
     <div>
+      <Header />
       トップページ画面
     </div>
   )
