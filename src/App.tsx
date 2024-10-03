beea2ed6e246a27d8ebc0c495818109c3a7b8d68
com12300000 <com12300000@gmail.com>
2024-10-04 01:23:55 +0900

    ポストページへのジャンプボタン作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 import TopPage from './components/TopPage';
 import MyPage from './components/MyPage';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import CreatePost from './components/CreatePost';
-import ContextProvider from './components/ContextProvider';
+import ContextProvider, { useAuth } from './components/ContextProvider';
+import { ProtectedRouteProps } from './interface/Interface';
 
 
 function App() {
+
+  const ProtectedRoute: React.FC<ProtectedRouteProps> = ({element}) => {
+    const currentUser = useAuth()
+    console.log('Current User:', currentUser); 
+    return currentUser ? <>{element}</> : <Navigate to='/login' />
+  }
+
   const routes = createBrowserRouter([
     {
-       path: '/', 
+      path: '/', 
       element: <TopPage />
     },
     { 
       path: '/mypage/:uid',
-       element: <MyPage />
+      element: <ProtectedRoute element={<MyPage />} />
     },
     { 
       path: '/login', 
---
+++ src/components/PostButton.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import './css/PostButton.css'
+
+const PostButton = () => {
+    const navigate = useNavigate()
+
+    const jumpCreatePost = () => {
+        navigate('/post')
+    }
+  return (
+    <div className='navbutton' onClick={jumpCreatePost}>
+        <span></span>
+        <span></span>
+    </div>
+  )
+}
+
+export default PostButton
