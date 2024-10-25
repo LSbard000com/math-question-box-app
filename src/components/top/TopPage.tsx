a4d036dd3d1275969ae2a8642f9fb011e2b546bf
com12300000 <com12300000@gmail.com>
2024-10-25 21:41:29 +0900

    トップのCSS微調整

--- src/components/top/TopPage.tsx
+++ src/components/top/TopPage.tsx
@@ -1,18 +1,31 @@
 import React from 'react'
+import '../css/TopPage.css'
 import Header from '../public/Header'
 import PostButton from '../public/PostButton'
 import PageTitle from '../public/PageTitle'
 import AllPosts from './AllPosts'
 import Footer from '../public/Footer'
+import Filter from './Filter'
 
 const TopPage = () => {
   return (
-    <div>
-      <Header />
-      <PageTitle title='トップページ' />
+    <div className='top-page'>
       <PostButton />
-      <AllPosts />
-      <Footer />
+      <div className='header'>
+        <Header />
+        <PageTitle title='トップページ' />
+      </div>
+      <div className='main'>
+        <div className='filter'>
+          <Filter />
+        </div>
+        <div className='all-posts'>
+          <AllPosts />
+        </div>
+      </div>
+      <div className='footer'>
+        <Footer />
+      </div>
     </div>
   )
 }
