e4927a41c3a23bcdb9c50d823599a995ac1530f1
com12300000 <com12300000@gmail.com>
2024-11-06 16:33:32 +0900

    テキストエンコーダーの定義

---
+++ src/types/text-encoding.d.ts
@@ -0,0 +1,6 @@
+// src/types/text-encoding.d.ts
+declare module '@sinonjs/text-encoding' {
+    export const TextEncoder: typeof globalThis.TextEncoder;
+    export const TextDecoder: typeof globalThis.TextDecoder;
+  }
+  
