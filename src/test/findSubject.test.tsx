7107b468f68c6c811a1041e21f11aff42911e333
com12300000 <com12300000@gmail.com>
2024-11-08 19:15:10 +0900

    findSubjectテスト追加

--- src/test/findSubject.test.tsx
+++ src/test/findSubject.test.tsx
@@ -4,4 +4,8 @@ describe('findSubject関数', () => {
     test('OK', () => {
         expect(findSubject('u101')).toBe('数論')
     })
+
+    test('OK', () => {
+        expect(findSubject('O00')).toBe('その他')
+    })
 })
